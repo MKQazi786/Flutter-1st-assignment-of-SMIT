3c1fd44ea0964d6911a555cea45ca84647deb699
Muhammad khubaib <120232076+MKQazi786@users.noreply.github.com>
2024-06-07 22:14:01 +0500

    Add files via upload
    This is 1st assignment of flutter in SMIT

---
+++ index.dart
@@ -0,0 +1,20 @@
+void main(){
+  calculateArea();
+  CxF();
+}
+
+void calculateArea() { 
+    var length = 2;
+    var breadth = 3;
+    var area = length * breadth;
+
+    print ('You give me your length is $length and breadth is $breadth and I convert into area. Your area is $area');
+}
+
+void CxF() {
+  
+    var C = 37;
+    var F = ((C) * 9/5) + 32;
+    
+    print ('your Celsius is $C in Fehrenheit is $F');
+}
